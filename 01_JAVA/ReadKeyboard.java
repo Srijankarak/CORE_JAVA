f507d5d96ebdae0adfea6f9ac5ca934ae1f17cbc
srijan karak <srijankarak2001@gmail.com>
2026-01-11 23:45:32 +0530

    first java code of this repo

---
+++ 01_JAVA/ReadKeyboard.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class ReadKeyboard{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x,y;
+        System.out.println("Enter 2 numbers");
+        x=sc.nextInt();
+        y=sc.nextInt();
+
+        int z =x+y;
+        System.out.println("Sum is: "+z);
+        sc.close();
+    }
+}
